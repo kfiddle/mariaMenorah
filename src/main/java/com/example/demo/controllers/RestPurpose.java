c753c7ed1550db6e0172c215483d5d412f65c622
kfiddle <kenjfiddle@gmail.com>
2021-07-19 18:59:14 -0400

    various

--- src/main/java/com/example/demo/controllers/RestPurpose.java
+++ src/main/java/com/example/demo/controllers/RestPurpose.java
@@ -3,9 +3,7 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Purpose;
 import com.example.demo.repositories.PurposeRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Collection;
@@ -23,6 +21,13 @@ public class RestPurpose {
         return (Collection<Purpose>) purposeRepo.findAll();
     }
 
+    @PostMapping("/add-purpose")
+    public Collection<Purpose> addAPurpose(@RequestBody Purpose purposeToAdd) {
+        if (!purposeRepo.existsByTitle(purposeToAdd.getTitle())) {
+            purposeRepo.save(purposeToAdd);
+        }
+        return (Collection<Purpose>) purposeRepo.findAll();
+    }
 
 
 }
--- src/main/java/com/example/demo/repositories/PurposeRepository.java
+++ src/main/java/com/example/demo/repositories/PurposeRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PurposeRepository extends CrudRepository<Purpose, Long> {
 
+    boolean existsByTitle(String title);
 }
